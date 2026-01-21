931b9859d2c0c66108b62c428eefd3106604c9c8
Pranshu-2177 <pranshukalathia383@gmail.com>
2026-01-21 14:29:01 +0530

    Add files via upload

---
+++ avgtotal.py
@@ -0,0 +1,14 @@
+def avg():
+
+    s1 = int(input("First Subject: "))
+    s2 = int(input("Second Subject: "))
+    s3 = int(input("Third Subject: "))
+
+    average = (s1+s2+s3)/3
+
+    total = s1 + s2 + s3
+
+    print("Average: ", average)
+    print("Total: ", total)
+
+avg()
