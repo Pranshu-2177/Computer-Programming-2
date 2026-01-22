ca673c17b8e9fb0b7494ef033da4bf236d99f100
Pranshu-2177 <pranshukalathia383@gmail.com>
2026-01-22 15:35:09 +0530

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+def rect():
+
+    l = int(input("Enter length: ")
+    b = int(input("Enter Breadth: ")
+
+    if(l*b > l+b):
+            print("Area is greater than perimeter")
+    else:
+        print("Area is not greater than perimeter")
+
+rect()
