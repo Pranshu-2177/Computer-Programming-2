fc7d712b966f606450843e7ea9521d73738a5051
Pranshu-2177 <pranshukalathia383@gmail.com>
2026-01-22 15:37:49 +0530

    Add files via upload

---
+++ numtowords.py
@@ -0,0 +1,42 @@
+num = int(input("Enter a number (1–19): "))
+
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3:
+    print("Three")
+elif num == 4:
+    print("Four")
+elif num == 5:
+    print("Five")
+elif num == 6:
+    print("Six")
+elif num == 7:
+    print("Seven")
+elif num == 8:
+    print("Eight")
+elif num == 9:
+    print("Nine")
+elif num == 10:
+    print("Ten")
+elif num == 11:
+    print("Eleven")
+elif num == 12:
+    print("Twelve")
+elif num == 13:
+    print("Thirteen")
+elif num == 14:
+    print("Fourteen")
+elif num == 15:
+    print("Fifteen")
+elif num == 16:
+    print("Sixteen")
+elif num == 17:
+    print("Seventeen")
+elif num == 18:
+    print("Eighteen")
+elif num == 19:
+    print("Nineteen")
+else:
+    print("Invalid input! Enter a number between 1 and 19.")
